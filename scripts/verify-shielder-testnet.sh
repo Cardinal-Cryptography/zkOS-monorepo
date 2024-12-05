da1550d4340b0068879ff3caafbd6adf8ddc5de4
Maksym Zub <kroistsmeh@gmail.com>
2024-12-05 17:27:50 +0100

    upd

--- scripts/verify-shielder-testnet.sh
+++ scripts/verify-shielder-testnet.sh
@@ -3,13 +3,15 @@
 set -euo pipefail
 
 LIBRARIES=$(cat broadcast/Shielder.s.sol/2039/run-latest.json | jq ".libraries | flatten[]")
-CONTRACT_ADDRESS=$(cat broadcast/Shielder.s.sol/2039/run-latest.json \
-    | jq -r '.transactions | .[] | select(.contractName=="Shielder") | .contractAddress')
+IMPL_CONTRACT_ADDRESS=$(cat broadcast/Shielder.s.sol/2039/run-latest.json \
+    | jq -r '.transactions | .[] | select(.transactionType=="CREATE") | select(.contractName=="Shielder") | .contractAddress')
+PROXY_CONTRACT_ADDRESS=$(cat broadcast/Shielder.s.sol/2039/run-latest.json \
+    | jq -r '.transactions | .[] | select(.transactionType=="CREATE") | select(.contractName=="ERC1967Proxy") | .contractAddress')
 
-echo ${CONTRACT_ADDRESS} > shielder_address.txt
+echo ${PROXY_CONTRACT_ADDRESS} > shielder_address.txt
 
 forge verify-contract --rpc-url https://rpc.alephzero-testnet.gelato.digital \
     --verifier blockscout --verifier-url https://evm-explorer-testnet.alephzero.org/api \
     --libraries ${LIBRARIES} \
-    ${CONTRACT_ADDRESS} \
+    ${IMPL_CONTRACT_ADDRESS} \
     contracts/Shielder.sol:Shielder
