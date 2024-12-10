a280f97d2d02a2ee59cd76a5783dd7be16f57947
Filip Bielejec <fbielejec@gmail.com>
2024-12-10 15:00:26 +0100

    Gas compare (#40)

--- crates/integration-tests/src/shielder/calls/domain_separator.rs
+++ crates/integration-tests/src/shielder/calls/domain_separator.rs
@@ -1,27 +1,31 @@
-use alloy_primitives::U256;
-use rstest::rstest;
-use shielder_rust_sdk::permit2::get_domain_separator;
+#[cfg(test)]
+mod tests {
 
-use crate::{
-    permit2,
-    shielder::deploy::{deployment, Deployment},
-};
+    use alloy_primitives::U256;
+    use rstest::rstest;
+    use shielder_rust_sdk::permit2::get_domain_separator;
 
-#[rstest]
-fn domain_separator(mut deployment: Deployment) {
-    let expected = deployment
-        .evm
-        .call(
-            deployment.contract_suite.permit2,
-            permit2::domain_separator_calldata(),
-            None,
-            None,
-        )
-        .expect("Permit2 DOMAIN_SEPARATOR call failed")
-        .output;
+    use crate::{
+        permit2,
+        shielder::deploy::{deployment, Deployment},
+    };
 
-    assert_eq!(
-        get_domain_separator(U256::from(1), deployment.contract_suite.permit2).to_vec(),
-        expected
-    );
+    #[rstest]
+    fn domain_separator(mut deployment: Deployment) {
+        let expected = deployment
+            .evm
+            .call(
+                deployment.contract_suite.permit2,
+                permit2::domain_separator_calldata(),
+                None,
+                None,
+            )
+            .expect("Permit2 DOMAIN_SEPARATOR call failed")
+            .output;
+
+        assert_eq!(
+            get_domain_separator(U256::from(1), deployment.contract_suite.permit2).to_vec(),
+            expected
+        );
+    }
 }
