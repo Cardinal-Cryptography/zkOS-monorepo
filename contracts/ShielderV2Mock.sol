1862f02ab46c74b5b8c5dcb5f1fe790a16a44810
Jan Kuczma <63134918+JanKuczma@users.noreply.github.com>
2024-12-11 10:50:57 +0100

    ZK-589: Oz upgrades tests (#41)
    Co-authored-by: Filip Bielejec <fbielejec@gmail.com>
    Co-authored-by: Maksym Zub <kroistsmeh@gmail.com>

---
+++ contracts/ShielderV2Mock.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { MerkleTree } from "./MerkleTree.sol";
+import { Nullifiers } from "./Nullifiers.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/**
+ * @title ShielderV2Mock
+ * @author CardinalCryptography
+ * @dev This is a mock version on ShielderV2 for testing purposes only.
+ *      Most of the functionality has been removed for clarity.
+ *
+ * @custom:oz-upgrades-from Shielder
+ * @custom:oz-upgrades-unsafe-allow external-library-linking
+ */
+contract ShielderV2Mock is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    MerkleTree,
+    Nullifiers
+{
+    uint256 public mockStateVariable;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @dev required by the OZ UUPS module
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function initialize(
+        address initialOwner,
+        uint256 _mockVariable
+    ) public initializer {
+        __Ownable_init(initialOwner);
+        __Pausable_init();
+        __MerkleTree_init();
+        _pause();
+
+        mockStateVariable = _mockVariable;
+    }
+
+    function reinitialize(uint256 _mockVariable) public reinitializer(2) {
+        mockStateVariable = _mockVariable;
+    }
+}
--- scripts/Shielder.s.sol
+++ scripts/Shielder.s.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.14;
 
-import { Script, console2 } from "forge-std/src/Script.sol";
+import { Script, console2 } from "forge-std/Script.sol";
 
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { Shielder } from "../contracts/Shielder.sol";
