1d8b114b805754dfd6e97fcc55cbfd9f02a54858
Maksym Zub <kroistsmeh@gmail.com>
2025-01-07 12:22:47 +0100

    separate cryptoClient interface

--- ts/shielder-sdk-crypto/src/cryptoClient.ts
+++ ts/shielder-sdk-crypto/src/cryptoClient.ts
@@ -10,31 +10,47 @@ import {
   WithdrawValues
 } from "./index";
 
-export interface CryptoClient {
+export interface NewAccountCircuit {
   proveNewAccount(values: NewAccountValues): Promise<Proof>;
-
   verifyNewAccount(
     proof: Proof,
     pubInputs: NewAccountPubInputs
   ): Promise<boolean>;
+}
 
+export interface DepositCircuit {
   proveDeposit(values: DepositValues): Promise<Proof>;
-
   verifyDeposit(proof: Proof, pubInputs: DepositPubInputs): Promise<boolean>;
+}
 
+export interface WithdrawCircuit {
   proveWithdraw(values: WithdrawValues): Promise<Proof>;
-
   verifyWithdraw(proof: Proof, pubInputs: WithdrawPubInputs): Promise<boolean>;
+}
 
-  proveAndVerifyMerkle(): Promise<Proof>;
-
+export interface Hasher {
   poseidonHash(inputs: Scalar[]): Promise<Scalar>;
+}
 
+export interface SecretManager {
   getSecrets(id: Scalar, nonce: number): Promise<ShielderActionSecrets>;
+}
 
+export interface Converter {
   privateKeyToScalar(hex: `0x${string}`): Promise<Scalar>;
+}
 
+export interface NoteTreeConfig {
   treeHeight(): Promise<number>;
-
   arity(): Promise<number>;
 }
+
+// Main interface that combines all crypto-related functionality
+export interface CryptoClient
+  extends NewAccountCircuit,
+    DepositCircuit,
+    WithdrawCircuit,
+    Hasher,
+    SecretManager,
+    Converter,
+    NoteTreeConfig {}
