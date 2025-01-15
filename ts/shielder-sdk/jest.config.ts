54adf68f3a82b9e6148cb5f3a000b383797fffe9
Maksym Zub <kroistsmeh@gmail.com>
2025-01-15 02:05:27 +0100

    more tests and coverage

--- ts/shielder-sdk/jest.config.ts
+++ ts/shielder-sdk/jest.config.ts
@@ -9,6 +9,8 @@ const config: JestConfigWithTsJest = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
+  collectCoverageFrom: ["src/**/*.ts"],
+  coveragePathIgnorePatterns: ["src/_generated"],
   transform: {
     ...presetConfig.transform,
     "^.+\\.[tj]sx?$": [
