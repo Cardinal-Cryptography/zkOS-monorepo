736e422ad684a64abd1127895ce1bc7020adb8c7
Maksym Zub <kroistsmeh@gmail.com>
2025-02-19 09:54:33 +0100

    binding

--- crates/shielder_bindings/src/circuits/withdraw.rs
+++ crates/shielder_bindings/src/circuits/withdraw.rs
@@ -107,43 +107,6 @@ impl WithdrawCircuit {
     }
 
     #[allow(clippy::too_many_arguments)]
-    pub fn pub_inputs(
-        &self,
-        id: Vec<u8>,
-        nonce: Vec<u8>,
-        nullifier_old: Vec<u8>,
-        trapdoor_old: Vec<u8>,
-        account_balance_old: Vec<u8>,
-        token_address: Vec<u8>,
-        path: Vec<u8>,
-        value: Vec<u8>,
-        nullifier_new: Vec<u8>,
-        trapdoor_new: Vec<u8>,
-        commitment: Vec<u8>,
-    ) -> Vec<u8> {
-        let knowledge = &WithdrawProverKnowledge {
-            id: vec_to_f(id),
-            nonce: vec_to_f(nonce),
-            nullifier_old: vec_to_f(nullifier_old),
-            trapdoor_old: vec_to_f(trapdoor_old),
-            account_old_balance: vec_to_f(account_balance_old),
-            token_address: vec_to_f(token_address),
-            path: vec_to_path(path),
-            withdrawal_value: vec_to_f(value),
-            nullifier_new: vec_to_f(nullifier_new),
-            trapdoor_new: vec_to_f(trapdoor_new),
-            commitment: vec_to_f(commitment),
-        };
-
-        let concat_vec = knowledge
-            .serialize_public_input()
-            .iter()
-            .flat_map(|value| field_to_bytes(*value))
-            .collect();
-        concat_vec
-    }
-
-    #[allow(clippy::too_many_arguments)]
     pub fn verify(
         &self,
         id_hiding: Vec<u8>,
