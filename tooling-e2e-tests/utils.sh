cdd6f7dbfe61714be80f468ea25c959b99802ede
Maksym Zub <kroistsmeh@gmail.com>
2025-02-21 12:39:02 +0100

    upd

--- tooling-e2e-tests/utils.sh
+++ tooling-e2e-tests/utils.sh
@@ -79,7 +79,6 @@ deploy_erc20_token() {
       --private-key "${DEPLOYER_PRIVATE_KEY}" \
       --broadcast \
       --constructor-args "${1}" "${2}" \
-      # 2> output.log \
     | grep 'Deployed to:' | awk '{print $NF}'
   )
 }
