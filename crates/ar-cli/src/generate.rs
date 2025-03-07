5e1df15cebad5cf7019d4c3c6d0125c22c71eaf1
filip <fbielejec@gmail.com>
2025-03-07 12:47:04 +0100

    wip generator

--- crates/ar-cli/src/generate.rs
+++ crates/ar-cli/src/generate.rs
@@ -1,1 +1,8 @@
-pub fn run() {}
+use rand::rngs::OsRng;
+use shielder_circuits::generate_keys;
+
+pub fn run() {
+    let mut rng = OsRng;
+
+    let (private_key_bits, public_key) = generate_keys(&mut rng);
+}
