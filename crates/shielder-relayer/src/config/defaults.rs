ebb55660896d364d632044244ef1ef5ece6e96d4
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-03-17 17:01:43 +0100

    consts

--- crates/shielder-relayer/src/config/defaults.rs
+++ crates/shielder-relayer/src/config/defaults.rs
@@ -13,10 +13,9 @@ pub const DEFAULT_RELAY_COUNT_FOR_RECHARGE: u32 = 20;
 pub const DEFAULT_TOTAL_FEE: &str = "100_000_000_000_000_000"; // 0.1 TZERO
 pub const DEFAULT_RELAY_GAS: u64 = 2_000_000; // an estimated amount of gas for a 'withdraw_native' call
 
-const ONE_MINUTE_IN_SECONDS: u64 = 60;
-pub const DEFAULT_PRICE_FEED_VALIDITY: Duration = Duration::from_secs(10 * ONE_MINUTE_IN_SECONDS);
-pub const DEFAULT_PRICE_FEED_REFRESH_INTERVAL: Duration =
-    Duration::from_secs(ONE_MINUTE_IN_SECONDS);
+const ONE_MINUTE: Duration = Duration::from_secs(60);
+pub const DEFAULT_PRICE_FEED_VALIDITY: Duration = 10 * ONE_MINUTE;
+pub const DEFAULT_PRICE_FEED_REFRESH_INTERVAL: Duration = ONE_MINUTE;
 pub const DEFAULT_SERVICE_FEE_PERCENT: u32 = 15;
 pub const DEFAULT_QUOTE_VALIDITY: Duration = Duration::from_secs(15);
 pub const DEFAULT_MAX_POCKET_MONEY: &str = "100_000_000_000_000_000"; // 0.1 TZERO
