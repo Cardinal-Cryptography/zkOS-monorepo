f1973223c540dafb670995b8423f6ddbfa1a9701
filip <fbielejec@gmail.com>
2025-03-17 16:10:37 +0100

    this is to big

--- crates/ar-cli/src/generate.rs
+++ crates/ar-cli/src/generate.rs
@@ -4,11 +4,11 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use endianess::Endianess;
 use log::{debug, info};
 use rand_chacha::{rand_core::SeedableRng, ChaCha12Rng};
 use shielder_circuits::{generate_keys, Fr, GrumpkinPointAffine};
 use thiserror::Error;
+use type_conversions::Endianess;
 
 use crate::cli::{self};
 
@@ -18,73 +18,6 @@ use crate::cli::{self};
 pub enum GeneratorError {
     #[error("Error writing")]
     Write(#[from] std::io::Error),
-}
-
-mod endianess {
-
-    use shielder_circuits::PrimeField;
-
-    pub trait Endianess {
-        fn to_bytes_le(&self) -> [u8; 32];
-        fn to_bytes_be(&self) -> [u8; 32];
-        #[cfg(test)]
-        fn from_bytes_be(be_bytes: [u8; 32]) -> Self;
-        #[cfg(test)]
-        fn from_bytes_le(be_bytes: [u8; 32]) -> Self;
-    }
-
-    impl<T> Endianess for T
-    where
-        T: PrimeField<Repr = [u8; 32]>,
-    {
-        fn to_bytes_le(&self) -> [u8; 32] {
-            self.to_repr()
-        }
-        fn to_bytes_be(&self) -> [u8; 32] {
-            let mut bytes = self.to_bytes_le();
-            bytes.reverse();
-            bytes
-        }
-        #[cfg(test)]
-        fn from_bytes_be(be_bytes: [u8; 32]) -> Self {
-            let mut le_bytes = be_bytes;
-            le_bytes.reverse();
-            Self::from_repr(le_bytes).expect("not a BE representation")
-        }
-        #[cfg(test)]
-        fn from_bytes_le(le_bytes: [u8; 32]) -> Self {
-            Self::from_repr(le_bytes).expect("not a LE representation")
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use shielder_circuits::{grumpkin, Fr, PrimeField};
-
-        use super::Endianess;
-
-        #[test]
-        fn bn254_fr_test() {
-            let element = Fr::from_u128(7);
-
-            let bytes_be = element.to_bytes_be();
-            assert_eq!(element, Endianess::from_bytes_be(bytes_be));
-
-            let bytes_le = element.to_bytes_le();
-            assert_eq!(element, Endianess::from_bytes_le(bytes_le));
-        }
-
-        #[test]
-        fn grumpkin_fr_test() {
-            let element = grumpkin::Fr::from_u128(7);
-
-            let bytes_be = element.to_bytes_be();
-            assert_eq!(element, Endianess::from_bytes_be(bytes_be));
-
-            let bytes_le = element.to_bytes_le();
-            assert_eq!(element, Endianess::from_bytes_le(bytes_le));
-        }
-    }
 }
 
 fn spit(dir: &Path, filename: &str, bytes: &[u8]) -> Result<(), std::io::Error> {
