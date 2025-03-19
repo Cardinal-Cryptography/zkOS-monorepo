0187f3a0a2eb4428633b808f2a02124dc637b0d5
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-03-19 13:00:18 +0100

    Fetching price test

--- crates/shielder-relayer/src/price_feed/fetching.rs
+++ crates/shielder-relayer/src/price_feed/fetching.rs
@@ -2,6 +2,7 @@ use rust_decimal::Decimal;
 use serde::Deserialize;
 use time::OffsetDateTime;
 
+/// This is the struct that we expect to receive at `https://api.diadata.org/v1/assetQuotation/`.
 #[derive(Clone, Debug, Deserialize)]
 pub struct PriceInfoFromProvider {
     #[serde(rename = "Price")]
@@ -28,90 +29,19 @@ pub async fn fetch_price(url: &str) -> Result<PriceInfoFromProvider, PriceFetchE
 
 #[cfg(test)]
 mod tests {
-    use alloy_primitives::address;
-    use rust_decimal::Decimal;
-    use shielder_relayer::{PriceProvider, Token, TokenKind};
-
     use super::fetch_price;
-    fn token_with_static_price() -> Token {
-        Token {
-            kind: TokenKind::Native,
-            price_provider: PriceProvider::Static(Decimal::ONE),
-        }
-    }
 
-    fn eth_url() -> Token {
-        Token {
-            kind: TokenKind::ERC20 {
-                address: address!("2222222222222222222222222222222222222222"),
-                decimals: 18,
-            },
-            price_provider: PriceProvider::Url(
-                "https://api.diadata.org/v1/assetQuotation/Ethereum/0x0000000000000000000000000000000000000000".to_string(),
-            ),
-        }
-    }
-
-    fn usdt_url() -> Token {
-        Token {
-            kind: TokenKind::ERC20 {
-                address: address!("1111111111111111111111111111111111111111"),
-                decimals: 6,
-            },
-            price_provider: PriceProvider::Url(
-                "https://api.diadata.org/v1/assetQuotation/Ethereum/0xdAC17F958D2ee523a2206206994597C13D831ec7".to_string(),
-            ),
-        }
-    }
-
-    #[tokio::test]
-    async fn can_fetch_static_price() {
-        fetch_price(&token_with_static_price())
-            .await
-            .expect("Should just read the price");
-    }
+    const ETH: &'static str =
+        "https://api.diadata.org/v1/assetQuotation/Ethereum/0x0000000000000000000000000000000000000000";
+    const USDT: &'static str =
+        "https://api.diadata.org/v1/assetQuotation/Ethereum/0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
     #[tokio::test]
     async fn can_fetch_price_from_url() {
-        fetch_price(&eth_url())
-            .await
-            .expect("Should connect to the feed and get price");
-    }
-
-    #[tokio::test]
-    async fn unit_price_is_correct() {
-        // ------------- ETH has 18 decimals. -------------
-        let price = fetch_price(&eth_url()).await.unwrap();
-        let expected_token_price = price.unit_price * Decimal::from(1_000_000_000_000_000_000u128);
-
-        // Define a 1% tolerance
-        let margin = expected_token_price * Decimal::from_i128_with_scale(1, 2);
-        let lower_bound = expected_token_price - margin;
-        let upper_bound = expected_token_price + margin;
-
-        // Assert that the actual price is within the allowed range
-        assert!(
-            price.token_price >= lower_bound && price.token_price <= upper_bound,
-            "Price out of acceptable range: expected ~{}, got {}",
-            expected_token_price,
-            price.token_price
-        );
-
-        // ------------- USDT has 6 decimals. -------------
-        let price = fetch_price(&usdt_url()).await.unwrap();
-        let expected_token_price = price.unit_price * Decimal::from(1_000_000u128);
-
-        // Define a 1% tolerance
-        let margin = expected_token_price * Decimal::from_i128_with_scale(1, 2);
-        let lower_bound = expected_token_price - margin;
-        let upper_bound = expected_token_price + margin;
-
-        // Assert that the actual price is within the allowed range
-        assert!(
-            price.token_price >= lower_bound && price.token_price <= upper_bound,
-            "Price out of acceptable range: expected ~{}, got {}",
-            expected_token_price,
-            price.token_price
-        );
+        for token in &[ETH, USDT] {
+            fetch_price(token)
+                .await
+                .expect("Should connect to the feed and get price");
+        }
     }
 }
