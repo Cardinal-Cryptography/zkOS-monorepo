54917aacd2bc7d8fea29f947dd33ab2f4bfe773a
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-03-24 12:34:45 +0200

    Fixes

--- crates/shielder-cli/src/recovery.rs
+++ crates/shielder-cli/src/recovery.rs
@@ -24,6 +24,7 @@ use crate::app_state::AppState;
 
 pub async fn recover_state(app_state: &mut AppState, token: Token) -> Result<()> {
     let shielder_user = app_state.create_shielder_user();
+    app_state.ensure_account_exist(token);
     let AppState {
         accounts,
         node_rpc_url,
