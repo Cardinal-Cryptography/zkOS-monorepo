6a27aca394605b6da1f7be1e17dea94f9a37e408
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-03-28 23:58:45 +0100

    Fix nightly

--- tooling-e2e-tests/utils.sh
+++ tooling-e2e-tests/utils.sh
@@ -60,6 +60,7 @@ mtzero() {
 deploy_shielder_contracts() {
   SHIELDER_CONTRACT_ADDRESS=$(
     PRIVATE_KEY="${DEPLOYER_PRIVATE_KEY}" \
+    NETWORK="${NODE_RPC_URL}" \
 	  ./scripts/deploy-shielder.sh \
       2> output.log \
     | grep 'Shielder deployed at:' | awk '{print $NF}')
