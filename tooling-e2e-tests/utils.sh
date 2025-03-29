32bd5027b021ad8fc99b697fc8d017d807f06ec7
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-03-29 11:48:25 +0100

    Fix nightly (#194)

--- tooling-e2e-tests/utils.sh
+++ tooling-e2e-tests/utils.sh
@@ -60,6 +60,7 @@ mtzero() {
 deploy_shielder_contracts() {
   SHIELDER_CONTRACT_ADDRESS=$(
     PRIVATE_KEY="${DEPLOYER_PRIVATE_KEY}" \
+    NETWORK="${NODE_RPC_URL}" \
 	  ./scripts/deploy-shielder.sh \
       2> output.log \
     | grep 'Shielder deployed at:' | awk '{print $NF}')
@@ -164,19 +165,6 @@ erc20_balance() {
 #### RELAYER #######################################################################################
 ####################################################################################################
 start_relayer() {
-  if [[ -z "${TOKEN_CONFIG:-}" ]]; then
-      TOKEN_CONFIG=$(cat <<EOF
-      [
-        {
-          "kind":"Native",
-          "price_provider":{"Static":1}
-        }
-      ]
-EOF
-      )
-    export TOKEN_CONFIG
-  fi
-
   cd "${ROOT_DIR}/crates/shielder-relayer/"
   make run &>> output.log
   cd "${ROOT_DIR}"
