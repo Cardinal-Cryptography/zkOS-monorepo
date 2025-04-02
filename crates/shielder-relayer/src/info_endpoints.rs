950a3c4f7b7d7cf669e15abcdb73c72187361dc7
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-04-02 12:20:55 +0200

    Dedicated module

---
+++ crates/shielder-relayer/src/info_endpoints.rs
@@ -0,0 +1,36 @@
+use axum::{extract::State, response::IntoResponse, Json};
+use shielder_relayer::TokenKind;
+
+use crate::AppState;
+
+/// Get the address to which relay fees should be sent.
+#[utoipa::path(
+    get,
+    path = "/fee_address",
+    responses((status = 200, body = String))
+)]
+pub async fn fee_address(state: State<AppState>) -> impl IntoResponse {
+    state.fee_destination.to_string()
+}
+
+/// Get information about the supported tokens.
+#[utoipa::path(
+    get,
+    path = "/supported_tokens",
+    responses((status = 200, body = [TokenKind]))
+)]
+pub async fn supported_tokens(state: State<AppState>) -> impl IntoResponse {
+    Json(
+        state
+            .token_config
+            .iter()
+            .map(|t| t.kind)
+            .collect::<Vec<_>>(),
+    )
+}
+
+/// Get upper limit for pocket money.
+#[utoipa::path(get, path = "/max_pocket_money", responses((status = 200, body = String)))]
+pub async fn max_pocket_money(state: State<AppState>) -> impl IntoResponse {
+    Json(state.max_pocket_money.to_string())
+}
