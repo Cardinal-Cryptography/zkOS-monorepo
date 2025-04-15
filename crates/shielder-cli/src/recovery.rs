2d7901da6e9799b246c612907f3dd5237f72b864
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-04-15 13:41:02 +0200

    CLI: recover ERC20 (#213)

--- crates/shielder-cli/src/recovery.rs
+++ crates/shielder-cli/src/recovery.rs
@@ -13,8 +13,9 @@ use shielder_contract::{
     events::get_event,
     providers::create_simple_provider,
     ShielderContract::{
-        depositNativeCall, newAccountNativeCall, withdrawNativeCall, Deposit, NewAccount,
-        ShielderContractEvents, Withdraw,
+        depositERC20Call, depositNativeCall, newAccountERC20Call, newAccountNativeCall,
+        withdrawERC20Call, withdrawNativeCall, Deposit, NewAccount, ShielderContractEvents,
+        Withdraw,
     },
 };
 use tracing::{error, info};
@@ -118,13 +119,19 @@ async fn try_get_shielder_event_for_tx(
     block_hash: BlockHash,
 ) -> Result<Option<ShielderContractEvents>> {
     let tx_data = tx.input();
-    let maybe_action = if newAccountNativeCall::abi_decode(tx_data, true).is_ok() {
+    let maybe_action = if newAccountNativeCall::abi_decode(tx_data, true).is_ok()
+        || newAccountERC20Call::abi_decode(tx_data, true).is_ok()
+    {
         let event = get_event::<NewAccount>(provider, tx.tx_hash(), block_hash).await?;
         Some(ShielderContractEvents::NewAccount(event))
-    } else if depositNativeCall::abi_decode(tx_data, true).is_ok() {
+    } else if depositNativeCall::abi_decode(tx_data, true).is_ok()
+        || depositERC20Call::abi_decode(tx_data, true).is_ok()
+    {
         let event = get_event::<Deposit>(provider, tx.tx_hash(), block_hash).await?;
         Some(ShielderContractEvents::Deposit(event))
-    } else if withdrawNativeCall::abi_decode(tx_data, true).is_ok() {
+    } else if withdrawNativeCall::abi_decode(tx_data, true).is_ok()
+        || withdrawERC20Call::abi_decode(tx_data, true).is_ok()
+    {
         let event = get_event::<Withdraw>(provider, tx.tx_hash(), block_hash).await?;
         Some(ShielderContractEvents::Withdraw(event))
     } else {
