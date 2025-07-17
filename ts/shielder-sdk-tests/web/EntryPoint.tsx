4621ff9a7ce3b854013d7fbc04fa68c73209edbc
Maksym Zub <kroistsmeh@gmail.com>
2025-07-17 13:24:17 +0200

    add crypto client option to sdk tests

--- ts/shielder-sdk-tests/web/EntryPoint.tsx
+++ ts/shielder-sdk-tests/web/EntryPoint.tsx
@@ -9,20 +9,17 @@ import {
   type ShielderClientConfig
 } from "@cardinal-cryptography/shielder-sdk";
 import type { CryptoClient } from "@cardinal-cryptography/shielder-sdk-crypto";
-import { initWasmWorker } from "@cardinal-cryptography/shielder-sdk-crypto-wasm";
-// import { initWasmWorker } from "@cardinal-cryptography/shielder-sdk-crypto-wasm-light";
 import type { GlobalConfigFixture } from "@tests/playwrightFixtures/globalConfig";
 import {
   type ShielderTestFixture,
   setupShielderTest
 } from "./fixtures/shielderTest/setup";
-
-import newAccountParamsUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/new_account/params.bin?url";
-import newAccountPkUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/new_account/pk.bin?url";
-import depositParamsUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/deposit/params.bin?url";
-import depositPkUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/deposit/pk.bin?url";
-import withdrawParamsUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/withdraw/params.bin?url";
-import withdrawPkUrl from "@cardinal-cryptography/shielder-sdk-crypto-wasm/keys/withdraw/pk.bin?url";
+import { initFullWasm, initLightWasm } from "./cryptoClient";
+import {
+  checkNitroAttestation,
+  cryptoClientType,
+  proverServerUrl
+} from "./envConfig";
 
 declare global {
   interface Window {
@@ -46,43 +43,19 @@ declare global {
   }
 }
 
-async function fetchArrayBuffer(url: string): Promise<Uint8Array> {
-  return await fetch(url).then((r) => r.bytes());
-}
-
 function EntryPoint() {
   useEffect(() => {
     const initialize = async () => {
-      const newAccountParams = await fetchArrayBuffer(newAccountParamsUrl);
-      const newAccountPk = await fetchArrayBuffer(newAccountPkUrl);
-      const depositParams = await fetchArrayBuffer(depositParamsUrl);
-      const depositPk = await fetchArrayBuffer(depositPkUrl);
-      const withdrawParams = await fetchArrayBuffer(withdrawParamsUrl);
-      const withdrawPk = await fetchArrayBuffer(withdrawPkUrl);
       // test fixtures initialization
       window.testFixtures = window.testFixtures || {};
       window.testFixtures.setupHappyTest = setupShielderTest;
       // Wasm crypto client initialization
       window.wasmCryptoClient = window.wasmCryptoClient || {};
-      window.wasmCryptoClient.cryptoClient = initWasmWorker(
-        "multi",
-        {
-          paramsBuf: newAccountParams,
-          pkBuf: newAccountPk
-        },
-        {
-          paramsBuf: depositParams,
-          pkBuf: depositPk
-        },
-        {
-          paramsBuf: withdrawParams,
-          pkBuf: withdrawPk
-        }
-      );
-      // window.wasmCryptoClient.cryptoClient = initWasmWorker(
-      //   "http://51.20.132.7:3000",
-      //   true
-      // );
+      console.log("Initializing crypto client with type:", cryptoClientType);
+      window.wasmCryptoClient.cryptoClient =
+        cryptoClientType === "wasm-full"
+          ? initFullWasm()
+          : initLightWasm(proverServerUrl!, checkNitroAttestation);
       // Expose shielder utilities
       window.shielder = window.shielder || {};
       window.shielder.createShielderClient = createShielderClient;
---
+++ ts/shielder-sdk-tests/web/envConfig.ts
@@ -0,0 +1,6 @@
+export const cryptoClientType =
+  (import.meta.env.VITE_CRYPTO_CLIENT_TYPE as "wasm-full" | "wasm-light") ||
+  "wasm-full";
+export const proverServerUrl = import.meta.env.VITE_PROVER_SERVER_URL || "";
+export const checkNitroAttestation =
+  import.meta.env.VITE_CHECK_NITRO_ATTESTATION === "true";
